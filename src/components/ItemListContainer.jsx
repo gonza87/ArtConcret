fa859c36f058c828e03aa7cb474279ecb8b54f36
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-05-24 11:09:36 -0300

    imagenesmacetasJson

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,53 +1,8 @@
-import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
 function ItemListContainer() {
   return (
     <div className="container containerListProducts">
-      <Card
-        className="cardProduct"
-        style={{ width: "18rem", background: "#E6E6E9" }}
-      >
-        <Card.Img variant="top" src="./img/caritanordica.png" />
-        <Card.Body>
-          <Card.Title>Maceta Carita Nórdica</Card.Title>
-          <Card.Text>500$</Card.Text>
-          <span className="btnAddCart">AÑADIR AL CARRITO</span>
-        </Card.Body>
-      </Card>
-      <Card
-        className="cardProduct"
-        style={{ width: "18rem", background: "#E6E6E9" }}
-      >
-        <Card.Img variant="top" src="./img/caritanordica.png" />
-        <Card.Body>
-          <Card.Title>Maceta Carita Nórdica</Card.Title>
-          <Card.Text>500$</Card.Text>
-          <span className="btnAddCart">AÑADIR AL CARRITO</span>
-        </Card.Body>
-      </Card>
-      <Card
-        className="cardProduct"
-        style={{ width: "18rem", background: "#E6E6E9" }}
-      >
-        <Card.Img variant="top" src="./img/caritanordica.png" />
-        <Card.Body>
-          <Card.Title>Maceta Carita Nórdica</Card.Title>
-          <Card.Text>500$</Card.Text>
-          <span className="btnAddCart">AÑADIR AL CARRITO</span>
-        </Card.Body>
-      </Card>
-      <Card
-        className="cardProduct"
-        style={{ width: "18rem", background: "#E6E6E9" }}
-      >
-        <Card.Img variant="top" src="./img/caritanordica.png" />
-        <Card.Body>
-          <Card.Title>Maceta Carita Nórdica</Card.Title>
-          <Card.Text>500$</Card.Text>
-          <span className="btnAddCart">AÑADIR AL CARRITO</span>
-        </Card.Body>
-      </Card>
       <Card
         className="cardProduct"
         style={{ width: "18rem", background: "#E6E6E9" }}
