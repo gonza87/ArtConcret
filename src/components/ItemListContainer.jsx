e0d4fff799ebcae06a979d74e3b6dcbeb90caf41
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-05-31 11:43:52 -0300

    category

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import ItemList from "./ItemList";
 
 function ItemListContainer() {
   const [products, setProducts] = useState([]);
+  const { categoryId } = useParams();
 
   useEffect(() => {
     fetch("/data/products.json")
       .then((resp) => resp.json())
       .then((data) => {
-        setProducts(data);
+        if (categoryId) {
+          setProducts(data.filter((prod) => prod.category.id === categoryId));
+        } else {
+          setProducts(data);
+        }
       })
       .catch((error) => {
         console.error("Error al cargar los productos:", error);
