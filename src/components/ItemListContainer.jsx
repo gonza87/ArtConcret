562cb979fec9204e467d72c0e4aaebef36338946
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-05-31 09:00:14 -0300

    functionAsybc

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from "react";
 import ItemList from "./ItemList";
+import dataProducts from "../data/products.json";
 
 function ItemListContainer() {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(dataProducts);
+      }, 1000);
+    });
+  };
+
+  useEffect(() => {
+    getProducts()
+      .then((res) => {
+        setProducts(res);
+      })
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+      });
+  }, []);
+
+  console.log(products);
+
   return (
     <div className="container">
       <h2 className="text-center">Productos</h2>
