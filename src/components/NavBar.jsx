d1a05e80dac1c3e168e39aae8490fc6b35895017
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-05-31 12:48:12 -0300

    dropdown

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-
+import Dropdown from "react-bootstrap/Dropdown";
 import ShoppingCart from "./ShoppingCart";
 
 function NavBar() {
@@ -15,6 +15,21 @@ function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
+            <Dropdown>
+              <Dropdown.Toggle
+                className="menuNav"
+                variant=""
+                id="dropdown-basic"
+              >
+                Dropdown Button
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
             <Link className="menuNav" to="/">
               Tienda
             </Link>
