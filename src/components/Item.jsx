fddf02859d8ab59c8a9fd36f1f18381b88eae5c3
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-06-21 12:35:45 -0300

    featuredanimatecss

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { CartContext } from "../context/CartContext";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
+import "animate.css/animate.min.css";
 
 function Item({ product }) {
   const { addToCart } = useContext(CartContext);
@@ -13,6 +14,19 @@ function Item({ product }) {
       className="cardProduct"
       style={{ width: "18rem", background: "#E6E6E9" }}
     >
+      {product.featured && (
+        <button
+          type="button"
+          className="btn btn-warning m-auto animate__animated animate__infinite infinite animate__swing fw-bold"
+          style={{
+            position: "absolute",
+            top: "0",
+            left: "30%",
+          }}
+        >
+          En Oferta
+        </button>
+      )}
       <Link to={`/item/${product.id}`}>
         <Card.Img
           className="imgcardProduct"
