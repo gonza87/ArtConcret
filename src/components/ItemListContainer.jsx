66f68b7888bd54fba5fb89af07fa70907bf7bc6f
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-06-21 12:15:48 -0300

    filterOrderBy

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ItemList from "./ItemList";
+import Form from "react-bootstrap/Form";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../firebase/config";
 
@@ -8,7 +9,12 @@ function ItemListContainer() {
   const [products, setProducts] = useState([]);
   const [titleCategory, setTitleCategory] = useState("Productos");
   const { categoryId } = useParams();
+  const [orderBy, setOrderBy] = useState("");
 
+  const handleOrderBy = (e) => {
+    setOrderBy(e.target.value);
+  };
+  console.log(orderBy);
   useEffect(() => {
     const productsRef = collection(db, "products");
 
@@ -36,11 +42,38 @@ function ItemListContainer() {
     } else {
       setTitleCategory("Productos");
     }
+
+    if (orderBy === "priceAsc") {
+      const productsOrganized = [...products].sort((a, b) => b.price - a.price);
+      setProducts(productsOrganized);
+    } else if (orderBy === "priceDesc") {
+      const productsOrganized = [...products].sort((a, b) => a.price - b.price);
+      setProducts(productsOrganized);
+    }
   }, [categoryId]);
+
+  useEffect(() => {
+    if (orderBy === "priceAsc") {
+      const productsOrganized = [...products].sort((a, b) => b.price - a.price);
+      setProducts(productsOrganized);
+    } else if (orderBy === "priceDesc") {
+      const productsOrganized = [...products].sort((a, b) => a.price - b.price);
+      setProducts(productsOrganized);
+    } else if (orderBy === "featured") {
+      setProducts(products.filter((product) => product.featured === true));
+    }
+  }, [orderBy]);
 
   return (
     <div className="container">
       <h2 className="text-center mt-5 mb-5">{titleCategory}</h2>
+
+      <Form.Select aria-label="Default select example" onChange={handleOrderBy}>
+        <option>Ordenar por:</option>
+        <option value="priceDesc">Precio: de más bajo a más alto</option>
+        <option value="priceAsc">Precio: de más alto a más bajo</option>
+        <option value="featured">Relevancia</option>
+      </Form.Select>
       <div className=" containerListProducts">
         <ItemList products={products} />
       </div>
